79cd51b275e4047c988325a88b00453cb6a9e3e3
Riccardo Buscicchio <riccardo.buscicchio@unimib.it>
2023-11-09 08:27:46 +0100

    Update figures.py

--- scripts/figures.py
+++ scripts/figures.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Figure settings
 ### FIGURE SIZES
-fig_width_pt = 2*246.0  # Get this from LaTeX using \showthe\columnwidth
+fig_width_pt = 2*246.0  # Get this from LaTeX using \the\columnwidth (this is for prd)
 inches_per_pt = 1.0 / 72.27               # Convert pt to inch
 golden_mean = (np.sqrt(5) - 1.0) / 2.0         # Aesthetic ratio
 fig_width = fig_width_pt * inches_per_pt  # width in inches
@@ -16,9 +16,9 @@ vert_rect_size = [fig_width, 2*fig_height]
 vert_square_size = [fig_width, 2*fig_width]
 vert_long_size = [1.5 * fig_width, 2*fig_height]
 
+#You can use the constants above as standard figure sizes.
 
-
-
+# The parameters below are suitable for plots. You need to import them as rcparams
 params = {'axes.labelsize': 18,
           'axes.titlesize': 24,
           'font.size': 18,
@@ -33,3 +33,11 @@ params = {'axes.labelsize': 18,
           'figure.figsize': rect_size,
          }
 
+# To import them into matplotlib use
+# plt.rcParams.update(my_params)
+# If you create a figure using the sizes provided above
+# plt.figure(rect_size)
+# ...plot stuff...
+# Save it and get rid of white around the figure.
+# plt.savefig("plot.pdf", bbox_inches="tight")
+# I find this matching with my target size when I set the width of the plot in LaTeX as 0.8\columwidth
